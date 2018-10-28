8dace6a32d89a8865a1f9d6d8e77fc8abdacabff
Kenny <kennethichoi@gmail.com>
2018-10-27 20:29:35 -0700

    Responsive fixes for all breakpoints. Click away to close for smaller screen widths.

--- scripts/app.js
+++ scripts/app.js
@@ -2,20 +2,48 @@
 
 // Set DOM variables
 let menuOpen = document.querySelector('.btn__link')
-    sidebar = document.querySelector('.wrapper__sidebar');
+    sidebar = document.querySelector('.wrapper__sidebar')
+    hamburger = document.querySelector('.btn__menu');
 
 // Event Listeners
 menuOpen.addEventListener('click', function() {
     if(sidebar.classList.contains('open')) {
         sidebar.style.display = 'none';
         sidebar.classList.toggle('open');
-        document.querySelector('.btn__link span').textContent = 'Click Me!';
+        hamburger.classList.toggle('btn-active');
+        // document.querySelector('.btn__link span').textContent = 'Click Me!';
     } else {
         sidebar.style.display = 'block';
         sidebar.classList.toggle('open');
-        document.querySelector('.btn__link span').textContent = 'Click Me Again!';
+        hamburger.classList.toggle('btn-active');
+        // document.querySelector('.btn__link span').textContent = 'Click Me Again!';
     }
     
 });
 
 // console.log(sidebar.classList.contains('.open'));
+
+
+// If browser vw is less than 1280px, then the off-canvas sidebar should appear on top of content, not pushing content to the side
+
+// Event listener for click off-sidebar to close menu
+
+window.addEventListener('click', function(e) {
+    // console.log('clicked!');
+    console.log(e);
+    if (window.innerWidth <= 768 && document.querySelector('.wrapper__sidebar.open')) {
+        if(e.clientX > 310) {
+            sidebar.style.display = 'none';
+            sidebar.classList.toggle('open');
+            hamburger.classList.toggle('btn-active');
+        }
+    } else if(window.innerWidth <= 1280 && document.querySelector('.wrapper__sidebar.open')) {
+        if(e.offsetX > 420) {
+            // console.log('clicked on main body!');
+            sidebar.style.display = 'none';
+            sidebar.classList.toggle('open');
+            hamburger.classList.toggle('btn-active');
+        }
+    }
+    
+});
